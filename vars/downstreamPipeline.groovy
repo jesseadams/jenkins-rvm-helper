b223e5d19154745d31a1d346d3849d110386426e
Mayowa Aladeojebi <mayowa.aladeojebi@stelligent.com>
2018-08-03 11:38:40 -0500

    Make Downstream deploy in same workspace

--- vars/downstreamPipeline.groovy
+++ vars/downstreamPipeline.groovy
@@ -1,31 +1,24 @@
 import pipeline.helpers.RVMHelper
 
 def call(String deployEnv, Map config) {
-  node {
-    try {
-      stage('Checkout SCM') {
-        rvm = new RVMHelper()
-        rvm.setup(config.rvmVersion, "${config.projectName}-" + deployEnv.toLowerCase())
-        env.deployment_id = sh(returnStdout: true, script: 'echo $(date +%Y%m%d%H%M%S)-$(uuidgen | cut -d - -f 1)').trim()
-      }
+  stage("${deployEnv} Environment Deploy") {
+    rvm = new RVMHelper()
+    rvm.setup(config.rvmVersion, "${config.projectName}-" + deployEnv.toLowerCase())
+    env.deployment_id = sh(returnStdout: true, script: 'echo $(date +%Y%m%d%H%M%S)-$(uuidgen | cut -d - -f 1)').trim()
+  }
 
-      stage("Setup ${deployEnv} Container Environment Variables") {
-        // Setup Container Environment Variables
-        rvm.rake("setup:secrets DEPLOY_ENV=${deployEnv}")
-      }
+  stage("Setup ${deployEnv} Container Environment Variables") {
+    // Setup Container Environment Variables
+    rvm.rake("setup:secrets DEPLOY_ENV=${deployEnv}")
+  }
 
-      stage("Deploy ${deployEnv} ${config.projectDescription} ALB") {
-        // Deploy Application Load Balancer
-        rvm.rake("deploy:alb DEPLOY_ENV=${deployEnv}")
-      }
+  stage("Deploy ${deployEnv} ${config.projectDescription} ALB") {
+    // Deploy Application Load Balancer
+    rvm.rake("deploy:alb DEPLOY_ENV=${deployEnv}")
+  }
 
-      stage("Deploy ${deployEnv} ${config.projectDescription} Container") {
-        // Deploy Container to ECS
-        rvm.rake("deploy:container DEPLOY_ENV=${deployEnv}")
-      }
-    }
-    catch (exc) {
-      slackSend color: "#DD3021", message: "Build Failed - ${env.JOB_NAME} ${env.BUILD_NUMBER} (<${env.BUILD_URL}|Open>)"
-    }
+  stage("Deploy ${deployEnv} ${config.projectDescription} Container") {
+    // Deploy Container to ECS
+    rvm.rake("deploy:container DEPLOY_ENV=${deployEnv}")
   }
 }
